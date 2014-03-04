b16eaa4ed1eb08fbae5180dff1b1ac733b752473
ruby <ruby@puppet-agent.(none)>
2014-03-03 21:51:59 -0500

    Ruby Test

---
+++ test.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+#print using puts
+puts "Hello World"
+
+puts "============================"
+#append String 
+puts "Append," << "String"
+
+puts "============================"
+#repeat Text 11 times
+test=" God Krishna, "
+puts test * 11 
+
+puts "============================"
+#repeat test 5 times
+10.times {print test}
+
+
+puts "============================"
+#insert shell command
+puts "Hey !  I am running " + `ruby --version`
+
+puts "============================"
